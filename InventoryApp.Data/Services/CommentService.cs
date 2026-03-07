60810b966b6de13c767d54a5c25091fc50782146
RashfiTabassum <rashfi2004@gmail.com>
2026-03-07 15:18:17 +0600

    Added discussion tab

---
+++ InventoryApp.Data/Services/CommentService.cs
@@ -0,0 +1,50 @@
+﻿using InventoryApp.Data.Context;
+using InventoryApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryApp.Data.Services
+{
+    public class CommentService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CommentService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<List<Comment>> GetByInventoryAsync(int inventoryId)
+        {
+            return await _db.Comments
+                .Include(c => c.Author)
+                .Where(c => c.InventoryId == inventoryId)
+                .OrderBy(c => c.CreatedAt)
+                .Select(c => new Comment
+                {
+                    Id = c.Id,
+                    Content = c.Content,
+                    CreatedAt = c.CreatedAt,
+                    InventoryId = c.InventoryId,
+                    AuthorId = c.AuthorId,
+                    Author = c.Author
+                })
+                .ToListAsync();
+        }
+
+        public async Task<Comment> AddCommentAsync(
+            int inventoryId, string authorId, string content)
+        {
+            var comment = new Comment
+            {
+                Content = content,
+                InventoryId = inventoryId,
+                AuthorId = authorId,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            _db.Comments.Add(comment);
+            await _db.SaveChangesAsync();
+            return comment;
+        }
+    }
+}
