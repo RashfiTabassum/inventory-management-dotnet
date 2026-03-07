6803ce60007f2f5b9a61c49cec15621c33a1dbb8
RashfiTabassum <rashfi2004@gmail.com>
2026-03-07 19:51:57 +0600

    added theme switcher

---
+++ InventoryApp.Web/wwwroot/app.js
@@ -0,0 +1,18 @@
+﻿window.themeHelper = {
+    getCookie: function (name) {
+        const match = document.cookie.match(
+            new RegExp('(^| )' + name + '=([^;]+)'));
+        return match ? match[2] : null;
+    },
+    setCookie: function (name, value) {
+        document.cookie = name + '=' + value
+            + ';path=/;max-age=31536000';
+    },
+    applyTheme: function (isDark) {
+        if (isDark) {
+            document.body.classList.add('dark');
+        } else {
+            document.body.classList.remove('dark');
+        }
+    }
+};
